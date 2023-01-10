0ea6e2869ae57760366a75c65604369d39a899ae
dystopianctzn <kinda.toxic11@gmail.com>
2023-01-10 04:18:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Mainclass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите свой возраст:");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите своё имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Ваш возраст {0} и ваше имя {1}", age, name);
+
+        Console.WriteLine("Какой ваш любимый день недели?");
+        DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+        Console.WriteLine("Ваш любимый день недели это {0}", day);
+        
+        Console.WriteLine("Введите дату своего рождения:");
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Ваша дата рождения {0}", birthdate);
+
+        Console.ReadLine();
+       
+
+
+
+
+
+
+    }
+}
